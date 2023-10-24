d7e0ffdbecd0010149112399ebe012bc69afd287
Javed Khan <javedkhan@Javeds-MacBook-Pro.local>
2023-10-24 17:05:58 +0530

    node_modules added

--- middlewares/sendEmail.js
+++ middlewares/sendEmail.js
@@ -2,21 +2,41 @@ const nodemailer = require('nodemailer');
 
 // Configure nodemailer with your email service provider's settings
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    host: 'smtp.hostinger.com',
+    port: 465,
     auth: {
-        user: 'stone.schmeler75@ethereal.email',
-        pass: 'q1sMaSYmQK2F3cmz8h'
+        user: 'info@indplatform.in',
+        pass: 'Javed@617161'
     }
 });
 
 // Function to send an email with the specified subject and message
-const sendEmail = (to, subject, message) => {
+const sendEmail = (to, subject, otp) => {
+    const htmlMessage = `
+    <div style="font-family: Helvetica,Arial,sans-serif;width:100%;overflow:auto;line-height:2">
+   <div style="margin:50px auto;width:100%;padding:20px 0">
+      <div style="border-bottom:1px solid #eee">
+         <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">IndPlatform</a>
+      </div>
+      <p style="font-size:1.1em">Hi,</p>
+      <p>Thank you for choosing IndPlatform. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
+      <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">${otp}</h2>
+      <p style="font-size:0.9em;">Regards,<br />IndPlatform</p>
+      <hr style="border:none;border-top:1px solid #eee" />
+      <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
+         <p>IndPlatform Inc</p>
+         <p>1600 Amphitheatre Parkway</p>
+         <p>California</p>
+      </div>
+    </div>
+    </div>
+    `;
+
     const mailOptions = {
-        from: 'leora.jaskolski@ethereal.email', // Sender's email address
-        to, // Recipient's email address
+        from: 'info@indplatform.in',
+        to,
         subject,
-        text: message,
+        html: htmlMessage,
     };
 
     return new Promise((resolve, reject) => {
